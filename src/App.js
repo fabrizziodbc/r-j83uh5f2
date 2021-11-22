a412d0113dd53baa48ba7fbd6916559f82ed0fcb
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-22 12:50:43 -0500

    coutner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      contador: 0,
+    };
+    this.OnWriteHandle = this.OnWriteHandle.bind(this);
+  }
+  OnWriteHandle = (event) => {
+    console.log("value: ", this.state.value);
+    console.log("contador: ", this.state.contador);
+
+    this.setState({
+      value: event.target.value,
+    });
+    this.setState({
+      contador: event.target.value.length,
+    });
+  };
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea
+          rows="3"
+          onChange={this.OnWriteHandle}
+          value={this.state.value}
+        ></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
